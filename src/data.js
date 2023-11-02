d396a9e00b54228d22090364f1bc334bd8555987
jontygrbac <s3659145@student.rmit.edu.au>
2023-11-02 11:57:41 +1100

    Added skills page

---
+++ src/data.js
@@ -0,0 +1,2 @@
+export const skills = ["React", "Javascript", "Java" ,"SpringBoot", "C and C++","C#", "Python","Elixir","Elixir Phoenix", "UI/UX Tools", "Unix/Linux","TCP/IP and general network programming"
+,"SQL", "NoSQL"];
